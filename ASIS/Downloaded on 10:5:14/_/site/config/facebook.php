ad4b51890ebfa5300c67b4d4c4875a4cf94b9e8f
Jason Tan <jason.chris.tan@gmail.com>
2014-10-16 16:06:36 -0700

    deleted

--- ASIS/Downloaded on 10:5:14/_/site/config/facebook.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$config['facebook_user_id'] 		= 'usc.asis';
-	$config['facebook_app_id'] 			= '223919930968760';
-	$config['facebook_api_secret'] 		= '222d937ee8e472dfcdd7b1ca68b46644';
-	$config['facebook_default_scope']	= 'user_photos'; // E.G 'read_stream,birthday,users_events,rsvp_event'
-	$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
--- ASIS/Downloaded on 10:5:14/_/site/views/welcome.php
+++
@@ -1,7 +0,0 @@
-<section class="container " id="welcome">
-	<div class="row">
-		<div class="twelvecol">
-		<?php $this->load->view("slider"); ?>
-		</div>
-	</div>
-</section>
