ec31cbbf310c25966091519d51954af28e09b666
Jason Tan <jason.chris.tan@gmail.com>
2015-03-09 22:40:27 -0700

    add KISSmetrics

--- ASIS/site/views/header.php
+++ ASIS/site/views/header.php
@@ -2,6 +2,21 @@
 <html lang="en">
 
 <head>
+
+	<script type="text/javascript">var _kmq = _kmq || [];
+var _kmk = _kmk || 'a77df8b300b73dd54d73a06a470f900cbb5feca0';
+function _kms(u){
+  setTimeout(function(){
+    var d = document, f = d.getElementsByTagName('script')[0],
+    s = d.createElement('script');
+    s.type = 'text/javascript'; s.async = true; s.src = u;
+    f.parentNode.insertBefore(s, f);
+  }, 1);
+}
+_kms('//i.kissmetrics.com/i.js');
+_kms('//doug1izaerwt3.cloudfront.net/' + _kmk + '.1.js');
+</script>
+
 	<meta charset="utf-8" />
 	<title>USC ASIS | Association of Indonesian Students</title>
 
